6491482ff4e4a981ada50ca372278295f1cd1afa
siddhib011 <siddhib011@gmail.com>
2026-03-24 13:21:24 +0530

    fixed solution

--- news-app/screens/SplashScreen.js
+++ news-app/screens/SplashScreen.js
@@ -1,17 +1,37 @@
-import LottieView from 'lottie-react-native';
-import {  View } from 'react-native';
-export const SplashScreen = () => {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <LottieView
-                Style={require('../assets/anim.json')}
+// npm install lottie-react-native
+import LottieView from "lottie-react-native";
+import { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
 
-                source={}
+export const SplashScreen = ({navigation}) => {
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.replace("Home");
+        }, 5000);
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <LottieView
+                style={styles.splashCard}
+                source={require('../assets/anim.json')}
                 autoPlay
                 loop
-
-
-                />
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    splashCard: {
+        width: 300,
+        height: 300
+    }
+})
